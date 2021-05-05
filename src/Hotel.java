dedd0830542515a2dec478f5be605481347d97dd
Ronak Shah <37ronakshah@gmail.com>
2021-05-04 22:34:16 -0400

    Code written to complete the hotel meal example.

--- src/Hotel.java
+++ src/Hotel.java
@@ -10,5 +10,8 @@ public class Hotel {
     public static void main(String[] args) {
         double groupTotalMealPrice = calculateTotalMealPrice(50, 0.10, 0.12);
         System.out.println("Your total meal price is " + groupTotalMealPrice);
+
+        double individualMealPrice = groupTotalMealPrice / 5;
+        System.out.println("Each person pays: "+ individualMealPrice);
     }
 }
